fd67c5f2383de76453d910309865591e778ae7b3
egorcouldlie <couldliegor@gmail.com>
2022-12-02 09:56:14 +0300

    gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //1 задание
+        //1 - 2 задание
         int first = 27897;
         byte second = 67;
         short third = -159;
@@ -13,5 +13,14 @@ public class Main {
         System.out.println("Значение переменной с типом long равно " + fourth + "!");
         System.out.println("Значение переменной с типом float равно " + fifth + "!");
         System.out.println("Значение переменной с типом double равно " + sixth + "!");
+        //Задание 3(вспомнил, что забыл создать папку в самом начале ((
+
+        var wholeCountOfStudents = 23 + 27 + 30;
+        var paperCountPerStudent = 480 / wholeCountOfStudents;
+        System.out.println("На каждого ученика рассчитано " + paperCountPerStudent + " листов бумаги");
+        //программа компилируется !
+
+        //задание 4
+
     }
 }
