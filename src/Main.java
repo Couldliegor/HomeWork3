0dd704cd5041ef04bff2fcf7e63b1e1425b526e7
egorcouldlie <couldliegor@gmail.com>
2022-12-02 09:49:15 +0300

    1-2 tasks

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,14 @@ public class Main {
         int first = 27897;
         byte second = 67;
         short third = -159;
-        long fourth = 987678965549l;
+        long fourth = 987678965549L;
         float fifth = 2.786f;
         double sixth = 27.12;
+        System.out.println("Значение переменной с типом int равно " + first + "!");
+        System.out.println("Значение переменной с типом byte равно " + second + "!");
+        System.out.println("Значение переменной с типом short равно " + third + "!");
+        System.out.println("Значение переменной с типом long равно " + fourth + "!");
+        System.out.println("Значение переменной с типом float равно " + fifth + "!");
+        System.out.println("Значение переменной с типом double равно " + sixth + "!");
     }
 }
