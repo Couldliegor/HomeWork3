7d68feaf32adfc45474861c1e38974398736a54d
egorcouldlie <couldliegor@gmail.com>
2022-12-02 09:38:54 +0300

    1-2 tasks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        //1 задание
+        int first = 27897;
+        byte second = 67;
+        short third = -159;
+        long fourth = 987678965549l;
+        float fifth = 2.786f;
+        double sixth = 27.12;
     }
 }
